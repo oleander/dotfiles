3eb2ad4375e4be9db1bdeeba93e27488a892282f
Linus Oleander <linus@oleander.nu>
2016-06-16 00:37:51 +0200

    Move all configs into the symlink dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:oleander/dotfiles.git"
 set :deploy_to, "~/.dotfiles"
 set :keep_releases, 5
 set :pty, true
-set :configs, ["gemrc", "gitignore", "gvimrc", "irbrc", "tmuxrc", "vimrc", "zshrc", "gitconfig", "hushlogin"]
+set :configs, `ls symlinks`.split(/\s+/)
 set :scm, :git
 set :git_strategy, Capistrano::Git::SubmoduleStrategy
 set :git_keep_meta, true
@@ -17,7 +17,7 @@ namespace :deploy do
   task :symlink do
     on roles(:server) do
       fetch(:configs).each do |config|
-        execute "ln", "-fs", File.join(release_path, config), "~/.#{config}"
+        execute "ln", "-fs", File.join(release_path, "symlinks", config), "~/.#{config}"
       end
     end
   end
@@ -27,6 +27,6 @@ end
 
 task :test do
   fetch(:configs).each do |config|
-    sh "ln", "-fs", File.join(Dir.pwd, config), "/Users/linus/.#{config}"
+    sh "ln", "-fs", File.join(Dir.pwd, "symlinks", config), "/Users/linus/.#{config}"
   end
 end
