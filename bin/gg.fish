5de3bb16ff7947f3ece01db2acfd0f024220f77f
Linus Oleander <linus@oleander.io>
2023-02-19 00:18:41 +0100

    Improve squash

--- bin/gg.fish
+++ bin/gg.fish
@@ -5,5 +5,26 @@ if not set -q argv[1]
   exit 1
 end
 
+# Get the current branch name
+set branch (git rev-parse --abbrev-ref HEAD)
+
+# Store the arguments in a var called $message
+set message $argv
+
+# If the branch name matches ANYTHING-1234, then prefix the commit message with ANYTHING-1234
+if echo $branch | grep -q -E '^[A-Z]+-[0-9]+'
+  # Unless the commit message $message already starts with ANYTHING-1234
+  if not echo $message | grep -q -E '^[A-Z]+-[0-9]+'
+    set message "$branch $message"
+  end
+end
+
+# Check if the --dry-run flag was passed anywhere in $args
+# if so, print the commit message and exit
+if echo $argv | grep -q -- --dry-run
+  echo "[Dry run] Commit message: $message"
+  exit 0
+end
+
 git add -A
-git commit -m "$argv"
+git commit -m "$message"
