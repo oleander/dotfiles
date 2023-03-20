d61ebc70278e4c6004cff588d5dd9b90241788a8
Linus Oleander <linus@oleander.io>
2023-03-20 16:07:55 +0100

    Better gm

--- bin/gm.fish
+++ bin/gm.fish
@@ -5,4 +5,16 @@ if not set -q argv[1]
     exit 1
 end
 
-git commit -m $argv
+# Move all arguments to a single string
+set message $argv
+
+# Ensure the first character is uppercase
+set message (echo $message | sed 's/^\(.\)/\u\1/')
+
+# Prefix the current name of the branch if the branch makes the following expression ABC-123
+set branch (git rev-parse --abbrev-ref HEAD)
+if echo $branch | grep -q -E '^[A-Z]+-[0-9]+$'
+    set message "$branch $message"
+end
+
+git commit -m $message
