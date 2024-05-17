c1a6a7a061bf7539414af3195c06e5256d254b9f
Linus Oleander <linus@oleander.io>
2024-05-17 02:30:47 +0200

    Update scripts to remove images along with links

--- configs/config/fabric/patterns/summarize_docs/remove-links.lua
+++ configs/config/fabric/patterns/summarize_docs/remove-links.lua
@@ -4,15 +4,14 @@ function Link(el)
 end
 
 function Image(el)
-  -- Return the content of the link without the link itself
-  return el.content
+  -- Return the content of the image alt text without the image itself
+  return el.caption
 end
 
--- Return the document unchanged if the element is not a Link
+-- Return the document unchanged if the element is not a Link or Image
 return {
   {
     Link = Link,
     Image = Image
-
   }
 }
