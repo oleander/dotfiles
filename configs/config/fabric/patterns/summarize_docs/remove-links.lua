f6b5114be8d68db26790d416edb001499d79605a
Linus Oleander <linus@oleander.io>
2024-05-22 10:19:17 +0200

    Update summarize_docs script & add aliases
    - Add pre-processing step to summarization script.
    - Modify remove-links.lua to handle Emph and Strong elements.
    - Update PATH in zshrc.
    - Add `help2` alias to `context-extractor`.

--- configs/config/fabric/patterns/summarize_docs/remove-links.lua
+++ configs/config/fabric/patterns/summarize_docs/remove-links.lua
@@ -1,17 +1,24 @@
 function Link(el)
-  -- Return the content of the link without the link itself
   return el.content
 end
 
 function Image(el)
-  -- Return the content of the image alt text without the image itself
   return el.caption
 end
 
--- Return the document unchanged if the element is not a Link or Image
+function Emph(el)
+  return el.content
+end
+
+function Strong(el)
+  return el.content
+end
+
 return {
   {
     Link = Link,
-    Image = Image
+    Image = Image,
+    Emph = Emph,
+    Strong = Strong,
   }
 }
