9398c40684917c9f16def493e0278907d11c3a3d
Linus Oleander <220827+oleander@users.noreply.github.com>
2024-10-23 02:39:54 +0200

    Remove obsolete brew and dotbot installation comments

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -e
+# #!/bin/bash -e
 
-if ! command -v brew &>/dev/null; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
+# if ! command -v brew &>/dev/null; then
+#   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# fi
 
-brew bundle install
-dotbot -c install.conf.yaml
+# brew bundle install
+# dotbot -c install.conf.yaml
