1fdf47479e6c4c1ef0aabae51483631037b303be
Linus Oleander <220827+oleander@users.noreply.github.com>
2024-11-08 17:19:54 +0100

    Refactor Docker setup and streamline linux.sh script

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,11 @@
 FROM mcr.microsoft.com/vscode/devcontainers/python:3.11
 
 VOLUME /root
-ENV TERM xterm-256color
 
 WORKDIR /root/.dotfiles
 
-RUN apt-get update
-
-RUN apt-get install -y zsh
-RUN chsh -s $(which zsh)
-
-RUN apt-get install -y curl
-RUN curl -sS https://starship.rs/install.sh | sh -s -- --yes
-# Used in both macos and linux
-RUN curl -L git.io/antigen > ~/.antigen.zsh
-
-RUN apt-get install -y git autojump vim
-
-RUN pip3 install dotbot
 COPY . .
-RUN dotbot -c install.conf.yaml
-
-RUN zsh -c "source ~/.zshrc"
+RUN ./install.sh
 
 SHELL ["/bin/zsh", "-i", "-c"]
 
--- scripts/linux.sh
+++ scripts/linux.sh
@@ -2,28 +2,10 @@
 
 export TERM=xterm-256color
 
-# Update packages
-apt-get update
-
-# Install Zsh and set it as the default shell
-apt-get install -y zsh
-# chsh -s $(which zsh)
-
-# Install curl and Starship prompt
-apt-get install -y curl
+sudo apt-get update
+sudo apt-get install -y zsh curl git autojump vim python3-pip
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
-
-# Download Antigen for Zsh
-curl -L git.io/antigen >~/.antigen.zsh
-
-# Install additional packages
-apt-get install -y git autojump vim
-
-# Install dotbot
 pip3 install dotbot
-
-# Run dotbot installation
+ln -s "$(pwd)" ~/.dotfiles
 dotbot -c install.conf.yaml
-
-# Source the Zsh configuration
 zsh -c "source ~/.zshrc"
