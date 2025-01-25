f33e6c304115494824d1d6d36391896407d7271c
Linus Oleander <220827+oleander@users.noreply.github.com>
2025-01-25 08:40:23 +0100

    Update script to install git-ai and adjust PATH

--- script/alpine.sh
+++ script/alpine.sh
@@ -5,17 +5,22 @@ DOTBOT_CONFIG=install.conf.yaml
 
 cd "$DOTFILES_DIR"
 
+echo "Updating submodules"
 git submodule update --init --recursive
 
+echo "Installing dotbot"
 export PATH="$HOME/.local/bin:$PATH"
 pip3 install --user dotbot
-dotbot -c "$DOTBOT_CONFIG"
+dotbot -c install.conf.yaml
 
+echo "Installing autojump"
 cd ./autojump
 ./install.py
 cd "$DOTFILES_DIR"
 
+echo "Installing git-ai"
 unset RUSTC_WRAPPER
+export PATH="$HOME/.cargo/bin:$PATH"
 apk add --update cargo
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 cargo binstall git-ai
