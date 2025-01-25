b0782aa0918a3661734d01d83b6a43c41cd73bfe
Linus Oleander <220827+oleander@users.noreply.github.com>
2025-01-25 14:40:05 +0100

    Reorder autojump installation for sequence consistency

--- script/alpine.sh
+++ script/alpine.sh
@@ -12,14 +12,14 @@ export PATH="$HOME/.local/bin:$PATH"
 pip3 install --user dotbot --root-user-action
 dotbot -c install.conf.yaml
 
-echo "Installing autojump"
-cd ./autojump
-./install.py
-cd "$DOTFILES_DIR"
-
 echo "Installing git-ai"
 unset RUSTC_WRAPPER
 export PATH="$HOME/.cargo/bin:$PATH"
 apk add --update cargo
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 cargo binstall git-ai
+
+echo "Installing autojump"
+cd ./autojump
+./install.py
+cd "$DOTFILES_DIR"
