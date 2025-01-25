fdfdf6cca426c0149eb3fe11976415a4aebb8d0b
Linus Oleander <220827+oleander@users.noreply.github.com>
2025-01-25 19:19:06 +0100

    Add cargo-binstall script installation via curl

--- script/alpine.sh
+++ script/alpine.sh
@@ -22,7 +22,7 @@ echo "Installing git-ai"
 unset RUSTC_WRAPPER
 export PATH="$HOME/.cargo/bin:$PATH"
 apk add --update cargo
-cargo install cargo-binstall
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 cargo binstall -y --targets x86_64-unknown-linux-musl git-ai
 
 # echo "Installing autojump"
