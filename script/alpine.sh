e8247c000c94ef6432487159e7fb8681b8f1b676
Linus Oleander <220827+oleander@users.noreply.github.com>
2025-01-25 20:40:36 +0100

    Remove comments to enable autojump installation

--- script/alpine.sh
+++ script/alpine.sh
@@ -13,7 +13,7 @@ apk add --update cargo
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 cargo binstall -y --targets x86_64-unknown-linux-musl git-ai
 
-# echo "Installing autojump"
-# cd ./autojump
-# ./install.py
-# cd "$DOTFILES_DIR"
+echo "Installing autojump"
+cd ./autojump
+./install.py
+cd "$DOTFILES_DIR"
