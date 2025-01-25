45407775c7ad2788456053e1c4a895df115b8f4d
Linus Oleander <220827+oleander@users.noreply.github.com>
2025-01-25 08:13:42 +0100

    Add cargo-binstall to alpine script for git-ai installation

--- script/alpine.sh
+++ script/alpine.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -e
 
-unset RUSTC_WRAPPER
-
-# cargo install git-ai
-
 cd /share/.dotfiles
 git config pull.ff only
 git stash
@@ -13,5 +9,11 @@ git submodule update --init --recursive
 pip3 install dotbot --break-system-packages
 dotbot -c install.conf.yaml
 zsh --login || true
+
 cd /share/.dotfiles/autojump
 ./install.py
+
+unset RUSTC_WRAPPER
+apk add --update cargo
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+cargo binstall git-ai
