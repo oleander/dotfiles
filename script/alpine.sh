b7bea9c70f375cd705db9e2b5ea78d7d4732b574
Linus Oleander <220827+oleander@users.noreply.github.com>
2025-01-25 08:37:00 +0100

    Simplify alpine.sh script for clarity and maintenance

--- script/alpine.sh
+++ script/alpine.sh
@@ -1,31 +1,20 @@
-#!/bin/bash -e
+#!/bin/bash
 
-# Ensure the dotfiles directory exists
-cd /share/.dotfiles
+DOTFILES_DIR=/share/.dotfiles
+DOTBOT_CONFIG=install.conf.yaml
 
-# Ensures changes are stashed before pulling new changes
-git config pull.ff only
-git stash
-git pull
+cd "$DOTFILES_DIR"
 
-# Sync and update submodules
-# Do we need both?
-git submodule sync
 git submodule update --init --recursive
 
-# Install dotbot and run the install script to distribute the dotfiles
-pip3 install dotbot --break-system-packages
-dotbot -c install.conf.yaml
+pip3 install --user dotbot
 
-# Install autojump (submodule)
+dotbot -c "$DOTBOT_CONFIG"
+
 cd ./autojump
 ./install.py
+cd "$DOTFILES_DIR"
 
-# Refresh the shell to install zsh packages
-# Does not work
-zsh --login || true
-
-# Install git-ai
 unset RUSTC_WRAPPER
 apk add --update cargo
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
