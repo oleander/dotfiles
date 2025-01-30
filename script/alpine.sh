e0415ab2e0fddf7282e942c03f4c35ba86514cbd
Linus Oleander <220827+oleander@users.noreply.github.com>
2025-01-30 11:25:01 +0100

    Remove git-ai and autojump installation commands

--- script/alpine.sh
+++ script/alpine.sh
@@ -5,18 +5,3 @@ DOTFILES_DIR=/share/dotfiles
 cd "$DOTFILES_DIR"
 
 ./install
-
-echo "Installing git-ai"
-unset RUSTC_WRAPPER
-export PATH="$HOME/.cargo/bin:$PATH-"
-apk add --update cargo
-curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-cargo binstall -y --targets x86_64-unknown-linux-musl git-ai
-
-echo "Installing autojump"
-git clone https://github.com/wting/autojump.git /tmp/autojump
-cd /tmp/autojump
-chmod +x ./install.py || echo "Failed to chmod install.py"
-export SHELL=/bin/zsh || echo "Failed to set SHELL"
-./install.py || echo "Failed to install autojump"
-cd "$DOTFILES_DIR"
