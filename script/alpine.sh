6d17a98317087878040d422d2510b7e3bc1a424d
Linus Oleander <220827+oleander@users.noreply.github.com>
2025-01-30 11:08:30 +0100

    Fix duplicate PATH entry in alpine.sh

--- script/alpine.sh
+++ script/alpine.sh
@@ -8,7 +8,7 @@ cd "$DOTFILES_DIR"
 
 echo "Installing git-ai"
 unset RUSTC_WRAPPER
-export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH-"
 apk add --update cargo
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 cargo binstall -y --targets x86_64-unknown-linux-musl git-ai
